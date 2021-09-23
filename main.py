60df29b66815f49728db9de8bde2100c55fa9587
Aeroz69 <72295917+Aeroz69@users.noreply.github.com>
2021-09-23 18:34:45 +0200

    Update and rename main.js to main.py

--- main.py
+++ main.py
@@ -10,14 +10,14 @@ if not os.getenv('DEVICE_ID') and \
           "If you're confused, re-watch the tutorial.")
     sys.exit()
 
-os.system('pip install -U schbot')
+os.system('pip install -U NIKEbot')
 os.system('clear')
 
-import schbot
+import NIKEbot
 
 asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
-client = schbot.PartyBot(
+client = NIKEbot.PartyBot(
     device_id=os.getenv('DEVICE_ID'),
     account_id=os.getenv('ACCOUNT_ID'),
     secret=os.getenv('SECRET')
